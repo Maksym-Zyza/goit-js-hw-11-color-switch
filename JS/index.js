74d912a09460494e9049d5a2b1ac6e15f50555e7
Maksym Zyza <mmziza88@gmail.com>
2020-12-16 22:26:13 +0200

    Добавлені базові файли

---
+++ JS/index.js
@@ -0,0 +1,5 @@
+// import colors from "./colors.js";
+
+const startBnRef = document.querySelector('button[data-action-start]')
+const startBnRef = document.querySelector('button[data-action-stop]')
+console.log(startBnRef);
